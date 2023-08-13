31a6740d66423293cb4e7d2211f01e679bdef579
harmonyRepo <137297636+harmonyRepo@users.noreply.github.com>
2023-08-13 22:36:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -18,8 +18,10 @@ const USER_CONFIG = {
 
   USERS: [
     {         
-      // 想要发送的人的名字
-      name: '秀宝',
+      
+
+        // 想要发送的人的名字
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oLDeE6ivpsSjyvvZ0_8BRlMMlI8M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,6 +31,7 @@ const USER_CONFIG = {
       holidaytts: true,
       CIBA: true,
       birthdayMessage: true,
+      
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
