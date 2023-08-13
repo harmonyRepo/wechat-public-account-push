a165a7cd987a3e8c7b07b5650154f17dad923c5b
harmonyRepo <137297636+harmonyRepo@users.noreply.github.com>
2023-08-13 23:07:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLDeE6ivpsSjyvvZ0_8BRlMMlI8M',
+      id: 'oLDeE6nIAV7Tkf_yi1luVjBTuj3c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'eii2aoQKMFjakBoa3xfDDaT85LrXp96aUY-irCz-huM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
